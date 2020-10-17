f5fe836f85077ae21a02023014f41a00c872bcd9
mgr <mgrojo@gmail.com>
2020-10-17 20:08:20 +0200

    Find the resource files inside an AppImage
    Set current directory to the APPDIR value path (set by the AppImage
    runtime). In this way we allow running from an AppImage and still find
    the resource files.

--- src/bingada.adb
+++ src/bingada.adb
@@ -1,9 +1,20 @@
 with Gtk.Main;
 with Q_Bingada;
 
+with Ada.Directories;
+with Ada.Environment_Variables;
+
 procedure Bingada is
 
 begin
+
+   -- Set current directory to the APPDIR value path. In this way we
+   -- allow running from an AppImage and still find the resource files.
+   --
+   if Ada.Environment_Variables.Exists ("APPDIR") then
+      Ada.Directories.Set_Directory
+        (Ada.Environment_Variables.Value ("APPDIR"));
+   end if;
 
    Gtk.Main.Init;
 
